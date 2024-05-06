d423d5fee0041036015fb402e13e15732f13713c
wsl-company <zys15664510728@gmail.com>
2024-05-06 10:36:04 +0800

    修复bot发送消息目标不是svr的bug

--- src/bot/login_bot.rs
+++ src/bot/login_bot.rs
@@ -10,7 +10,7 @@ use std::net::{IpAddr, Ipv4Addr};
  */
 pub fn bot_login() {
     let sock = UdpSocket::bind(String::from(cfg::BOT_01_ADDR)).expect("failed to bind addr");
-    let addr = cfg::BOT_01_ADDR.clone().parse::<SocketAddr>().unwrap();
+    let addr = cfg::SERVER_ADDR.clone().parse::<SocketAddr>().unwrap();
     pb::send_proto(sock.try_clone().unwrap(), addr.clone(), 10001, pb::gate::CsReqLogin {account: 11111, passwword: "11111".to_string()});
     println!("bot_login send CsReqLogin");
     // todo last 收听结果
@@ -21,7 +21,7 @@ pub fn bot_login() {
  */
 pub fn bot_register() {
     let sock = UdpSocket::bind(String::from(cfg::BOT_01_ADDR)).expect("failed to bind addr");
-    let addr = cfg::BOT_01_ADDR.clone().parse::<SocketAddr>().unwrap();
+    let addr = cfg::SERVER_ADDR.clone().parse::<SocketAddr>().unwrap();
     pb::send_proto(sock.try_clone().unwrap(), addr.clone(), 10003, pb::gate::CsReqRegister {account: 22222, passwword: "22222".to_string()});
     println!("bot_register: send CsReqRegister");
     // todo last 收听结果
