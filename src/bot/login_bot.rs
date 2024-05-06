c88c99f63007d78c9f77ce052c6d9d3b737fce35
wsl-company <zys15664510728@gmail.com>
2024-05-06 13:12:33 +0800

    注册bot监听结果回包

--- src/bot/login_bot.rs
+++ src/bot/login_bot.rs
@@ -8,7 +8,7 @@ use std::net::{IpAddr, Ipv4Addr};
 /**
  * @brief 测试登录
  */
-pub fn bot_login() {
+pub fn bot_login() -> Option<(UdpSocket, SocketAddr)> {
     let sock = UdpSocket::bind(String::from(cfg::BOT_01_ADDR)).expect("failed to bind addr");
     let saddr = cfg::SERVER_ADDR.clone().parse::<SocketAddr>().unwrap();
     pb::send_proto(sock.try_clone().unwrap(), saddr.clone(), 10001, pb::gate::CsReqLogin {account: 11111, passwword: "11111".to_string()});
@@ -20,22 +20,35 @@ pub fn bot_login() {
     println!("收到svr回包, proto = {}", proto);
     if proto != 10002 {
         println!("fatal: login recv must be 10002 !!!");
-        return;
+        return None;
     }
     let msg = pb::gate::CsRspLogin::decode(bytes.as_slice()).expect("failed to decode msg");
     println!("result = {}, err_code = {}, {}", msg.result, msg.error_code, if msg.result  {"登录成功"} else {"登录失败"});
-    return;
+    return Some((sock, saddr));
 }
 /**
  * @brief 测试注册
  */
-pub fn bot_register() {
+pub fn bot_register() -> Option<(UdpSocket, SocketAddr)>  {
     let sock = UdpSocket::bind(String::from(cfg::BOT_01_ADDR)).expect("failed to bind addr");
-    let addr = cfg::SERVER_ADDR.clone().parse::<SocketAddr>().unwrap();
-    pb::send_proto(sock.try_clone().unwrap(), addr.clone(), 10003, pb::gate::CsReqRegister {account: 22222, passwword: "22222".to_string()});
+    let saddr = cfg::SERVER_ADDR.clone().parse::<SocketAddr>().unwrap();
+    pb::send_proto(sock.try_clone().unwrap(), saddr.clone(), 10003, pb::gate::CsReqRegister {account: 33333, passwword: "33333".to_string()});
     println!("bot_register: send CsReqRegister");
-    // todo last 收听结果
+    // 收听结果
+    let mut buf: [u8; 1024] = [0u8; cfg::LISTEN_BUF_SIZE];
+    let (size, addr) = sock.recv_from(&mut buf).expect("login_bot::bot_register()");
+    let (proto, bytes) = pb::unpack_msg(&mut buf, size);
+    println!("收到svr回包, proto = {}", proto);
+    if proto != 10004 {
+        println!("fatal: login recv must be 10004 !!!");
+        return None;
+    }
+    let msg = pb::gate::CsRspRegister::decode(bytes.as_slice()).unwrap();
+    println!("result = {}, err_code = {}, {}", msg.result, msg.error_code, if msg.result  {"注册成功"} else {"注册失败"});
+    return Some((sock, saddr));
 }
-pub fn bot_select_role() {
+pub fn bot_select_role() -> Option<(UdpSocket, SocketAddr)>  {
+    let (sock,saddr) = bot_login().unwrap();
 
+    return None;
 }
--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -8,13 +8,15 @@
 pub mod gate_bot;
 pub mod login_bot;
 
+use std::net::SocketAddr;
+use std::net::UdpSocket;
 use std::thread;
 use std::time;
 
-pub fn run_bot(f: fn(), delay_time: u64) {
+pub fn run_bot(f: fn() -> Option<(UdpSocket, SocketAddr)>, delay_time: u64) {
     let _ = std::thread::spawn(move || {
         let sleep_duration = time::Duration::from_secs(delay_time);
         thread::sleep(sleep_duration);
-        f();
+        f().unwrap();
     });
 }
